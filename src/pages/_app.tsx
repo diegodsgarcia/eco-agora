92d1b10d2e20d6aec0a2e56ef5316cc8885796fb
diegodsgarcia <diego.ds.garcia@gmail.com>
2020-12-29 16:55:28 -0300

    chore: configure metatags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,9 +11,14 @@ function App({ Component, pageProps }: AppProps) {
     <ThemeProvider theme={theme}>
       <Head>
         <title>Eco Agora</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="shortcut icon" href="/ecovias-logo-512.png" />
+        <link rel="apple-touch-icon" href="/ecovias-logo-512.png" />
         <link rel="manifest" href="/manifest.json" />
+
+        <meta name="theme-color" content={theme.colors.primary} />
+        <meta property="og:url" content="https://eco-agora.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Eco agora" />
         <meta
           name="description"
           content="Aplicação utilizado para ver o estado atual das ecovias"
